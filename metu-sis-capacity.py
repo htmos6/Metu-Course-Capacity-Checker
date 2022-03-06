161d3aec7e1cc7e226f6a62318af49a664db8e1b
htmos6 <88316097+htmos6@users.noreply.github.com>
2022-03-06 22:20:45 +0300

    Update metu-sis-capacity.py

--- metu-sis-capacity.py
+++ metu-sis-capacity.py
@@ -6,8 +6,8 @@ from functions import scroll, is_there_empty_chair, find_course, play_music
 
 file_path = 'C:/Users/Legion/Desktop/Python/selenium-metu-sis/warning.mp3'
 
-def metu_course_capacity_questioning(url, course_code):
 
+def metu_course_capacity_questioning(url, course_code):
     s = Service('C:/Users/Legion/Desktop/Python/geckodriver-v0.30.0-win64/geckodriver.exe')
     driver = webdriver.Firefox(service=s)
     driver.maximize_window()
@@ -47,10 +47,11 @@ def metu_course_capacity_questioning(url, course_code):
         if results[1]: 
             print(results[0])
             play_music(file_path)
-    
+
+
 if __name__ == "__main__":
-    
     url_ee = "https://sis.metu.edu.tr/get.php?package=I0oVOF5fGqPn2HT2oK7dqD3NEfGk351-WCU4vddDFBZ5fqO5zHQf5lcLL_s70FniHLSrs6qnxpgp61Vuqu0_Yg#/?selectSemester=20212&selectProgram=567&submitSearchForm=Search&stamp=ddkd2XBH4hPEeWn6bQ9x_qWPVTug33AG-oU6vNN68XJD_xmG416j5wAA8LkKyrGkukXGHhwsvKXWUwnsMPrpyg"
     url_486 = "https://sis.metu.edu.tr/get.php?package=I0oVOF5fGqPn2HT2oK7dqD3NEfGk351-WCU4vddDFBZ5fqO5zHQf5lcLL_s70FniHLSrs6qnxpgp61Vuqu0_Yg#/?selectSemester=20212&selectProgram=460&submitSearchForm=Search&stamp=RzGYhMejUjYw8Azib2EeZWcGblUaacxj10CAIeBNLl5eSxZX2cZtihl2ZLtrDwO7S8jXSRzJRHnX7cJcmKQ4sg"
     metu_course_capacity_questioning(url_ee, course_code=5670312)
     metu_course_capacity_questioning(url_486, course_code=4600486)
+    

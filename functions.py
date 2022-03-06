bf5c31a704e6aa8a0e591d2dc8eb629194197835
htmos6 <88316097+htmos6@users.noreply.github.com>
2022-03-06 22:24:22 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,21 +2,24 @@ import time
 from pygame import mixer
 from selenium.webdriver.common.by import By
 
+
 def scroll(driver):
-    #javascript integrated code to python-selenium
+    # javascript integrated code to python-selenium
     lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-    match=False
-    while(match==False):
+    match = False
+    while match == False:
         lastCount = lenOfPage
         time.sleep(3)
         lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
         if lastCount == lenOfPage:
-            match=True
+            match = True
         time.sleep(3)
+
 
 def find_course(driver, course_code):
     courses = driver.find_elements(By.XPATH, "//tr[td[contains(text(), {})]]".format(course_code))
     return courses
+
 
 def is_there_empty_chair(course):
     sentence = course.text.split()
@@ -25,9 +28,10 @@ def is_there_empty_chair(course):
     used_capacity = int(sentence[-1])
 
     if total_capacity-used_capacity > 0:
-        return [result,True]
+        return [result, True]
     else:
-        return [result,False]
+        return [result, False]
+
 
 def play_music(file_path):
     mixer.init()
